4b3d6f59341964691fe3d2fcffca0e09a0ccd718
Human Zoid <humanhzzoid@gmail.com>
2022-07-02 00:38:36 +0300

    all done

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
-
+import java.util.Locale;
 public class Main {
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             task1(scanner);
             task2(scanner);
+            task3(scanner);
         }
     }
 
@@ -13,15 +14,34 @@ public class Main {
         System.out.println(" Введите фамилию : ");
         String lastName = scanner.next();
         System.out.println(" Введите отчество : ");
-        String middleName  = String.valueOf(scanner.next());
+        String middleName = scanner.next();
         System.out.println(" Введите имя : ");
-        String firstName  = String.valueOf(scanner.next());
+        String firstName = String.valueOf(scanner.next());
         String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println(fullName);
+        System.out.println(" ФИО сотрудника - "+ fullName);
     }
 
     public static void task2(Scanner scanner) {
-
+        System.out.println(" Введите фамилию : ");
+        String lastName = scanner.next();
+        System.out.println(" Введите отчество : ");
+        String middleName = String.valueOf(scanner.next());
+        System.out.println(" Введите имя : ");
+        String firstName = String.valueOf(scanner.next());
+        String fullName = lastName + " " + firstName + " " + middleName;
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника - " + fullName);
     }
 
+    public static void task3(Scanner scanner) {
+        System.out.println(" Введите фамилию : ");
+        String lastName = scanner.next();
+        System.out.println(" Введите отчество : ");
+        String middleName = scanner.next();
+        System.out.println(" Введите имя : ");
+        String firstName = String.valueOf(scanner.next());
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО до замены ё " + fullName);
+        System.out.println("ФИО после замены ё " + fullName.replace("ё","e"));
+    }
 }
